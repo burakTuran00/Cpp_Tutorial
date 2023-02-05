1079a135e1e3adcfd8d5aac4e74002f4fc3b5ab1
burakTuran00 <burakturan7410@gmail.com>
2023-02-05 21:15:57 +0300

    Variables

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World"<< endl;
+    std::cout << "Hello World \n";
+    std::cout << "I came from Earth" << endl; // \n is equals to endl;
     return 0;
 }
---
+++ Variable.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // integer (whole number)
+    int x;      // Declaration
+    x = 10;     // Assignment
+    int y = 10; // Combine both
+    int sum = x + y;
+    int age = 21;
+    int year = 2000;
+
+    std::cout << sum << std::endl;
+
+    // double (number inculing decimal)
+    double price = 10.95;
+    double temperature = 25.80;
+
+    // char (just single character )
+    char grade = 'A';
+    char initial = 'N';
+
+    // boolean (true or false)
+    bool isGround = false;
+    bool power = true;
+    bool forSale = true;
+
+    // sting (like text)
+    string name = "Jack";
+    string day = "Friday";
+    string snack = "Apple";
+    string street = "Victory St.";
+
+    // float (decimal numbers)
+    float taxt = 25.50f;
+    float version = 1.0f;
+    float litre = 1.8f;
+
+    return 0;
+}
