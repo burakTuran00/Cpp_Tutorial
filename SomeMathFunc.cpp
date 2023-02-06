39719d27b00e2a5012cb82ed708e3539cf421ba1
burakTuran00 <burakturan7410@gmail.com>
2023-02-06 12:05:33 +0300

    Math Functions

---
+++ SomeMathFunc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x = 3.14;
+    double y = 4;
+    double z;
+
+    z = max(x, y);
+
+    z = min(x, y);
+
+    z = pow(2, 3);
+
+    z = sqrt(9);
+
+    z = abs(-5);
+
+    z = round(x);
+
+    z = ceil(x); // up
+
+    z = floor(x); // down
+
+    return 0;
+}
