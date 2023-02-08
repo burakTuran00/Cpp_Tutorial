4766afe81d57605e681e25c3dfde63d4613509d6
burakTuran00 <burakturan7410@gmail.com>
2023-02-08 21:40:13 +0300

    String Methods

---
+++ StringMethods.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    if (name.length() > 10)
+    {
+        cout << "Your name cannot be over 10 characters." << endl;
+    }
+    else
+    {
+        cout << "Welcome" << endl;
+    }
+
+    if (name.empty()) // True or False
+    {
+        cout << "You did not enter your name!" << endl;
+    }
+    else
+    {
+        cout << "Hello" << endl;
+    }
+
+    name.clear();
+
+    name.append("@gmail.com");
+
+    name.at(0); // First character of name. Index of varieble.
+
+    name.insert(0, "@");
+
+    name.find(' '); // Give us the position of the first space;
+
+    name.erase(0, 3); // Erase with number index
+
+    
+
+    return 0;
+}
