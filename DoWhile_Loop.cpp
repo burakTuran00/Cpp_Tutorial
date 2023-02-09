279473fdb914abb0d289eca0b274a4e8bfbadfdc
burakTuran00 <burakturan7410@gmail.com>
2023-02-09 11:29:17 +0300

    Do While Loop

---
+++ DoWhile_Loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // do some block of codes first. Then repeat again if condition is true.
+    // This loop work once and if the condition is true the repeat again.
+    f
+    int number;
+
+    do
+    {
+        cout << "Enter a positive number: ";
+        cin >> number;
+
+    } while (number < 0);
+
+    cout << "The number is " << number << endl;
+
+    return 0;
+}
