b275b811089a05155d5ac5bae8e64fe00892ab24
burakTuran00 <burakturan7410@gmail.com>
2023-02-09 16:01:59 +0300

    Guess Number Game

---
+++ GuessTheNumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, guess, tries;
+
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+
+    cout << "------------------Number Guessing Game------------------" << endl;
+
+    do
+    {
+        cout << "Enter a guess between (1-100): ";
+        cin >> guess;
+
+        tries++;
+
+        if (guess > num)
+        {
+            cout << "High" << endl;
+        }
+        else if (guess < num)
+        {""
+            cout << "Low" << endl;
+        }
+        else
+        {
+            cout << "Correct" << endl;
+            cout << "You tried " << tries << " times." << endl;
+        }
+
+    } while (guess != num);
+
+    return 0;
+}
